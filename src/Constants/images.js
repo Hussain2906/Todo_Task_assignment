d31fa3856e8b4d4a29c6ecff7586062bdad4e029
Hussain2906 <huss0602029@gmail.com>
2025-01-16 00:30:49 +0530

    Download UI

--- src/Constants/images.js
+++ src/Constants/images.js
@@ -2,10 +2,12 @@ export const pic1 = require("../assets/images/pic1.jpg");
 export const pic2 = require("../assets/images/pic2.jpg");
 export const pic3 = require("../assets/images/pic3.jpg");
 export const picLogo = require("../assets/images/logo/fmbLogo.png");
+export const data = require("../assets/images/data1.jpg")
 
 export default {
     pic1,
     pic2,
     pic3,
-    picLogo,    
+    picLogo,
+    data,
 }
--- src/Views/HomeScreens/DownloadScreen.js
+++ src/Views/HomeScreens/DownloadScreen.js
@@ -1,15 +1,39 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import images from '../../Constants/images'
+import { ThameFont } from '../../Constants/theme'
+
+
+const dumyData = [
+  {
+    id:1,
+    title: 'Name of the PDF or any other Document',
+    image: images.data
+  }
+]
+
+const renderItem = ({item})=>(
+  <View style={{flexDirection:'row', backgroundColor:'yellow', height:80, width:'90%', justifyContent:'space-between', marginHorizontal:'5%', marginTop:'10%', padding:5, borderRadius:30, borderWidth:4, borderColor:'#001F3F'}}>
+    <View style={{justifyContent:'center',width:'50%'}}>
+      <Text style={{fontSize:15, fontFamily:ThameFont.PrimaryMeduim}}>{item.title}</Text>
+    </View>
+    <View style={{position:'absolute', right:0, paddingHorizontal:10}}>
+      <Image source={item.image} style={{width:60, height:70, borderRadius:40,}}/>
+    </View>
+  </View>
+)
 
 const DownloadScreen = () => {
   return (
-    <ScrollView>
       <SafeAreaView style={{backgroundColor:'white', height:'100%', width:'100%'}}>
-        <View>
-          <Text>Screen for Download</Text>
+        <View style={{backgroundColor:'#B9E5E8', height:'95%', width:'95%', marginHorizontal:10, marginTop:10, borderRadius:30, borderWidth:4, borderColor:'#001F3F'}}>
+          <FlatList 
+          data={dumyData}
+          renderItem={renderItem}
+          keyExtractor={(item=> item.id)}
+          />
         </View>
       </SafeAreaView>
-    </ScrollView>
   )
 }
 
